461312a8134323c6b7a7fe94fcaafbc1071a28fe
Bastien Dumont <bastien.dumont@etu.univ-grenoble-alpes.fr>
2023-03-07 11:55:01 +0100

    fichier du modèle mvc

---
+++ js/request.js
@@ -0,0 +1,15 @@
+/**
+ * La classe request pour construire l'objet 
+ * correspondant àà la requête de l'api
+ */
+class request {
+
+    _description;
+    _keywords;
+    _photographer;
+    _title;
+
+    constructor()
+
+
+}
