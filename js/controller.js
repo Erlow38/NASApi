5ae30a0c584f93692379c7911c40a5a8f985136a
Bastien Dumont <bastien.dumont@etu.univ-grenoble-alpes.fr>
2023-03-07 17:40:32 +0100

    début de controller et vue

---
+++ js/controller.js
@@ -0,0 +1,7 @@
+//Initialisation du modèle
+let request = new Request();
+
+// ### Initialisation des listeners ###
+// - Pour l'envoie de la recherche à l'api
+view.boutouSearch.addEventListener("click", function() {
+})
