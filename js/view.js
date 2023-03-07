0478b8d962ccfc16f107a575f5f577eb5689ca8f
dumontb <bastien.dumont@etu.univ-grenoble-alpes.fr>
2023-03-07 22:14:52 +0100

    Développement du modèle et début vue

---
+++ js/view.js
@@ -0,0 +1,8 @@
+//objet contenant la vue
+const view = {
+    // Barre de recherche de missions
+    searchBar: document.getElementsByClassName("searchbar"),
+
+    // Bouton de recherche
+    boutouSearch : document.getElementsByTagName("button"),
+}
