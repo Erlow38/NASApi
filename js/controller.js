e2bd23fc1d46de60dbc1189b8e6097f74f1e2417
Erlow <98266516+Erlow38@users.noreply.github.com>
2023-03-14 14:41:16 +0100

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -3,32 +3,13 @@ let recherche = new Recherche();
 
 //### Initialisation des events listeners ###
 // - Pour le bouton de recherche de misions
-view.missionSearchButton.addEventListener("click", function() {
+view.SearchButton.addEventListener("click", function() {
     console.log("ouaissss");
     //On récupère la valeur de la barre de recherche
-    let searchValue = view.missionSearchBar.value;
+    let searchValue = view.SearchBar.value;
     //On set la valeur de la recherche dans l'objet
     recherche.setTitle(searchValue);
     //On lance la recherche
     recherche.search();
 });
 
-// - Pour le bouton de recherche de photographes
-view.photographerSearchButton.addEventListener("click", function() {
-    //On récupère la valeur de la barre de recherche
-    let searchValue = view.photographerSearchBar.value;
-    //On set la valeur de la recherche dans l'objet
-    recherche.setPhotographer(searchValue);
-    //On lance la recherche
-    recherche.search();
-});
-
-// - Pour le bouton de recherche de mots clés
-view.keywordsSearchButton.addEventListener("click", function() {
-    //On récupère la valeur de la barre de recherche
-    let searchValue = view.keywordsSearchBar.value;
-    //On set la valeur de la recherche dans l'objet
-    recherche.setKeywords(searchValue);
-    //On lance la recherche
-    recherche.search();
-});
--- js/view.js
+++ js/view.js
@@ -1,20 +1,8 @@
 //objet contenant la vue
 const view = {
-    // Barre de recherche de missions
-    missionSearchBar: document.getElementById("mission-search-bar"),
+    // Barre de recherche 
+    SearchBar: document.querySelector("#search-bar"),
 
-    // Barre de recherche de photographes
-    photographerSearchBar: document.getElementById("photographer-search-bar"),
-
-    // Barre de recherche de mots clés
-    keywordsSearchBar: document.getElementById("keywords-search-bar"),
-
-    //Bouton de recherche de missions
-    missionSearchButton: document.getElementById("mission-search-button"),
-
-    // Bouton de recherche de photographes
-    photographerSearchButton: document.getElementById("photographer-search-button"),
-
-    // Bouton de recherche de mots clés
-    keywordsSearchButton: document.getElementById("keywords-search-button"),
+    // Bouton de recherche 
+    keywordsSearchButton: document.querySelector("#search-button"),
 }
