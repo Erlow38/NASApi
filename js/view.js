f06e6843adebf3858e6d76378e236482d93d1b29
Bastien Dumont <bastien.dumont@etu.univ-grenoble-alpes.fr>
2023-03-15 14:58:41 +0100

    élément p pour pas de résultat

--- js/view.js
+++ js/view.js
@@ -13,29 +13,34 @@ const view = {
     noResult: document.querySelector("#no-result"),
 
     updateFrom(recherche) {
-        //boucle de parcours de l'input
-        for (const [key, value] of Object.entries(recherche.getResults())) {
-            //Création de plusieurs éléments html pour afficher les résultats
-            let result = document.createElement("div");
-            let title = document.createElement("h2");
-            let description = document.createElement("p");
-            let image = document.createElement("img");
+        //Vérification qu'il y a des résultats
+        if (Object.keys(recherche.getResults()).length === 0) {
+            this.noResult.innerHTML = "Aucun résultat";
+        }else{
+            //boucle de parcours de l'input
+            for (const [key, value] of Object.entries(recherche.getResults())) {
+                //Création de plusieurs éléments html pour afficher les résultats
+                let result = document.createElement("div");
+                let title = document.createElement("h2");
+                let description = document.createElement("p");
+                let image = document.createElement("img");
 
-            //ajout des classes css
-            result.setAttribute("class", "result-content");
-            title.setAttribute("class", "result-title");
-            description.setAttribute("class", "result-description");
-            image.setAttribute("class", "result-image");
+                //ajout des classes css
+                result.setAttribute("class", "result-content");
+                title.setAttribute("class", "result-title");
+                description.setAttribute("class", "result-description");
+                image.setAttribute("class", "result-image");
 
-            //Ajout des enfants à la div principal
-            this.output.appendChild(result);
-            result.appendChild(title);
-            result.appendChild(description);
-            result.appendChild(image);
-            //ajout des valeurs dans les éléments html
-            title.innerHTML = key;
-            description.innerHTML = value.description;
-            image.setAttribute("src", value.url);
+                //Ajout des enfants à la div principal
+                this.output.appendChild(result);
+                result.appendChild(title);
+                result.appendChild(description);
+                result.appendChild(image);
+                //ajout des valeurs dans les éléments html
+                title.innerHTML = key;
+                description.innerHTML = value.description;
+                image.setAttribute("src", value.url);
+            }
         }
     },
 }
